9b8c74baaa114a2afb5e5bb6671e12d24407c9cc
l3043y <boreyc2013@gmail.com>
2021-10-28 11:55:29 +0700

    tweak

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 import http from 'k6/http';
+import { check } from 'k6';
 import { randomIntBetween, uuidv4 } from "https://jslib.k6.io/k6-utils/1.0.0/index.js";
-import { sleep } from 'k6';
-
+// import { sleep } from 'k6';
+const users = 4000;
 export const options = {
     scenarios: {
       constant_request_rate: {
         executor: 'constant-arrival-rate',
-        rate: 10000,
+        rate: users,
         timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
-        duration: '5m',
-        preAllocatedVUs: 10000, // how large the initial pool of VUs would be
+        duration: '3m',
+        preAllocatedVUs: users, // how large the initial pool of VUs would be
         // maxVUs: 200, // if the preAllocatedVUs are not enough, we can initialize more
       },
     },
   };
 
 export default function () {
-    const url = 'http://192.168.11.80/iot-server.php'; // make sure this is not production
+    const url = 'http://192.168.11.75/iot-server.php'; // make sure this is not production
     // const url = 'http://test.k6.io/login';
     const payload = JSON.stringify({
         "identifier": uuidv4(),
@@ -30,5 +31,10 @@ export default function () {
         },
     };
 
-    http.post(url, payload, params);
+    const res = http.post(url, payload, params);
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+        'record is saved': (r) =>
+            r.body.includes('Records added successfully!!'),
+    });
 }
